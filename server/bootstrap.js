2600cb394596c49614d0b70ef5e788bcedcb995d
Yvan Wibaux <yvan@evaneos.com>
2012-11-16 15:15:44 +0100

    add reset

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,7 +1,7 @@
 console.log('START');
 Meteor.startup(function () {
-  	//Tadas.remove({});
-  	//console.log('RESET');
+  	Tadas.remove({});
+  	console.log('RESET');
 	Tadas.insert({name: "Première tache!"});
 	console.log('INSERTED:'+Tadas.findOne().name);
 });
